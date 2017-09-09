2fb13954072c57656c900f668241d27bf122b9e8
BePsvPT <freedom@csie.io>
2017-09-09 15:22:34 +0800

    fix 趕走惡魔的方法 quest

--- npc/1063013.js
+++ npc/1063013.js
@@ -1,10 +1,35 @@
+// quest: 趕走惡魔的方法
+
 var status = -1;
 
 function action(mode, type, selection) {
-    if (cm.isQuestActive(2236)) {
-	cm.forceCompleteQuest(2236);
-	cm.removeAll(4032263);
-	cm.sendOk("Quest completed.");
-    }
-    cm.dispose();
+  if (! cm.isQuestActive(2236) || ! cm.haveItem(4032263)) {
+    return cm.dispose();
+  }
+
+  var data = cm.getQuestCustomData(2236) || '000000';
+  var index;
+  var mapId = cm.getMap().getId();
+  var maps = {
+    105050200: 0,
+    105060000: 1,
+    105070000: 2,
+    105090000: {top: 3, bottom: 4},
+    105090100: 5
+  };
+
+  if (105090000 !== mapId) {
+    index = maps[mapId];
+  } else if (cm.getChar().getPosition().getY() < 1000) {
+    index = maps[mapId].top;
+  } else {
+    index = maps[mapId].bottom;
+  }
+
+  if ('0' === data.charAt(index)) {
+    cm.gainItem(4032263, -1);
+    cm.setQuestCustomData(2236, data.substr(0, index) + '1' + data.substr(index + 1));
+  }
+
+  cm.dispose();
 }
