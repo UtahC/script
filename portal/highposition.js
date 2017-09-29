52ffe3213e675da70347dfc393d8136f6e79acd8
BePsvPT <freedom@csie.io>
2017-09-29 18:39:45 +0800

    fix 站在巔峰的人 quest

--- portal/highposition.js
+++ portal/highposition.js
@@ -1,36 +1,31 @@
 function enter(pi) {
-    if (pi.getPlayer().getLevel() < 15 || pi.isQuestFinished(29004)) {
-	return false;
+  if (pi.getPlayer().getLevel() < 15 || pi.isQuestFinished(29004)) {
+    return false;
+  }
+
+  if (!pi.isQuestActive(29004)) {
+    pi.forceStartQuest(29004);
+    pi.forceStartQuest(27017, 'enter=00000');
+    pi.setQuestInfo(27018, '0');
+    pi.forceStartQuest(27018);
+  }
+
+  //nautilus, kerning, ellinia, perion, orbis in that order
+  var index = [120000000, 103000000, 101010103, 102000000, 200080100].indexOf(pi.getMapId());
+
+  if (index !== -1) {
+    var data = pi.getInfoQuest(27017);
+
+    if (data.charAt(index + 6) === '0') {
+      var number = parseInt(pi.getQuestRecord(27018).getInfo()) + 1;
+
+      data = data.substring(0, index + 6) + '1' + data.substring(index + 7);
+
+      pi.updateInfoQuest(27017, data);
+      pi.setQuestInfo(27018, number);
+      pi.getPlayer().dropMessage(-1, '已完成 ' + number + "/5");
+      pi.getPlayer().dropMessage(-1, "稱號成就 - 登峰者");
+      pi.showQuestMsg("當前稱號 - 登峰者 " + number + "/5 完成");
     }
-    if (!pi.isQuestActive(29004)) {
-	pi.forceStartQuest(29004);
-	pi.updateInfoQuest(27017, "enter=00000");
-	pi.forceStartQuest(27018, "0");
-    }
-    //nautilus, kerning, ellinia, perion, orbis in that order
-    var quest = pi.getInfoQuest(27017);
-    var number = parseInt(pi.getQuestRecord(27018).getCustomData());
-    var new_quest = "enter=";
-    var maps = Array(120000000, 103000000, 101010103, 102000000, 200080100);
-    var changedd = false;
-    for (var i = 0; i < maps.length; i++) {
-	var changed = false;
-	if (pi.getPlayer().getMapId() == maps[i]) {
-	    if (quest.substring(i+6, i+7).equals("0")) { //+6 for "enter="
-		new_quest += "1";
-		changed = true;
-		changedd = true;
-	    }
-	}
-	if (!changed) {
-	    new_quest += quest.substring(i+6, i+7);
-	}
-    }
-    if (changedd) {
-	pi.updateInfoQuest(27017, new_quest);
-	pi.forceStartQuest(27018, number+1, true);
-	pi.getPlayer().dropMessage(-1, (number+1) +"/5 completed");
-	pi.getPlayer().dropMessage(-1, "稱號成就 - 登峰者");
-	pi.showQuestMsg("當前稱號 - 登峰者 " + (number+1) + "/5 完成");
-    }
+  }
 }
